304f0e1e1fc06f4a4c4c6951a1b12ea05e34b960
angel umwiza <angelumwiza21@gmail.com>
2023-10-16 14:08:41 +0300

    testing for factorial

---
+++ test/factorial.js
@@ -0,0 +1,25 @@
+const assert = require('chai').assert;
+
+// Define the factorial function
+function factorial(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
+
+// Describe the test case
+describe('Factorial', function() {
+  it('should return the factorial of the number', function() {
+    // Define the input and expected output
+    const num = 5;
+    const expectedOutput = 10;
+
+    // Call the factorial function and get the actual output
+    const actualOutput = factorial(num);
+
+    // Assert that the actual output matches the expected output
+    assert.equal(actualOutput, expectedOutput);
+  });
+});
---
+++ test/test.js
@@ -0,0 +1,70 @@
+// mypair
+// Monicah Mirembe
+
+
+const assert = require('chai').assert;
+
+describe('my test space', function () {
+    let num = 9;
+    let str = 'Angel';
+
+    it('check that num is numeric', function () {
+        assert.isNumber(num);
+    });
+
+    it('check that str is string', function () {
+        assert.isString(str);
+    });
+
+
+
+    //Test First TDD Cycle 1
+
+    function multiply (a, b) { return 1;
+    } 
+
+    it.skip('check the result of the multiply function', function () {
+        assert.equal(multiply(1, 1), 1);
+    });
+
+
+
+        // Test First TDD Cycle 2
+
+    function multiply (c, d) { return 4;
+    }
+
+    it('check my other function', function () {
+        assert.equal(multiply(2, 2),4)
+    });
+
+});
+
+// final assignment
+// Define the multiply function
+function multiply(a, b) {
+    return a * b;
+}
+
+describe('my test space', function () {
+    it('check the result of the multiply function with 1 and 1', function () {
+        assert.equal(multiply(1, 1), 1);
+    });
+
+    it('check the result of the multiply function with 2 and 2', function () {
+        assert.equal(multiply(2, 2), 4);
+    });
+
+    it('check the result of the multiply function with 3 and 3', function () {
+        assert.equal(multiply(3, 3), 9);
+    });
+
+    it('check the result of the multiply function with 4 and 4', function () {
+        assert.equal(multiply(4, 4), 16);
+    });
+
+    it('check the result of the multiply function with 23 and 45', function () {
+        assert.equal(multiply(23, 45), 23*45);
+    });
+
+});
